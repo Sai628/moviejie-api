d1391e47f9098240aa327d363032d4639fe0507e
Sai628 <sai100628@gmail.com>
2018-05-16 17:37:48 +0800

    update deploy etc

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ set -e
 
 cp ./etc/supervisor/moviejie_api.conf /etc/supervisor/conf.d/moviejie_api.conf
 supervisord -c /etc/supervisor/supervisord.conf
-supervisorctl start moviejie_api
 nginx -s reload
