0b57dc524bed8787fbc286662c841ce2d744928f
Sai628 <sai100628@gmail.com>
2018-05-17 17:06:19 +0800

    update gunicorn config file

--- etc/gunicorn_conf.py
+++ etc/gunicorn_conf.py
@@ -8,7 +8,7 @@ bind = '127.0.0.1:5001'
 backlog = 2048  # The maximum number of pending connections
 
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_connections = 1024
+worker_connections = 10240  # The maximum number of simultaneous clients. This setting only affects the Eventlet and Gevent worker types.
 max_requests = 10240  # The maximum number of requests a worker will process before restarting.
 keepalive = 2  # The number of seconds to wait for requests on a Keep-Alive connection
 
