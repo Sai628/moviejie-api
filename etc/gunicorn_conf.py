0f8c4b8a59c9648ac2a9c9b9b65a1844a5e115be
Sai628 <sai100628@gmail.com>
2018-05-18 12:42:42 +0800

    update config file

--- etc/gunicorn_conf.py
+++ etc/gunicorn_conf.py
@@ -12,14 +12,14 @@ worker_connections = 10240  # The maximum number of simultaneous clients. This s
 max_requests = 10240  # The maximum number of requests a worker will process before restarting.
 keepalive = 2  # The number of seconds to wait for requests on a Keep-Alive connection
 
-loglevel = 'info'
-accesslog = '/var/log/moviejie-api/gunicorn_stdout.log'
-errorlog = '/var/log/moviejie-api/gunicorn_stdout.log'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Forwarded-For}i)s"'
-
 secure_scheme_headers = {
     'X-SCHEME': 'https',
 }
 x_forwarded_for_header = 'X-Forwarded-For'
+
+loglevel = 'info'
+accesslog = '/var/log/moviejie-api/gunicorn_stdout.log'
+errorlog = '/var/log/moviejie-api/gunicorn_stdout.log'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'
 
 reload = True  # Restart workers when code changes
