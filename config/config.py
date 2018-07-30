6c2c6f6f9ec367574e879fcfca0cfc83f5fa0fad
Sai628 <sai100628@gmail.com>
2018-07-30 15:01:40 +0800

    update api domain & login cookies

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@
 CACHE_EXPIRE_TIME = 1800
 
 # API地址
-API_DOMAIN = 'https://moviejie.net'
+API_DOMAIN = 'http://moviejie.xyz'
 API_INDEX = API_DOMAIN  # 首页地址
 API_NEW_MOVIE = API_DOMAIN + "/new/movie"  # 最新电影地址
 API_NEW_TV = API_DOMAIN + "/new/tv"  # 最新电视剧地址
@@ -19,10 +19,9 @@ SIMULATE_HEADERS = {
 
 # 模拟已登录用户对应的Cookies
 LOGIN_COOKIES = {
-    '_ga': 'GA1.2.14896441.1509380435',
-    '_xsrf': '2|d68f7930|5509740495ec4f2d0885c6b0220e06d7|1516465214',
-    'Hm_lvt_85abf7488e97713e5a0e152b2ab2316d': '1516465217',
-    'user_slug': '"2|1:0|10:1516465397|9:user_slug|8:YzI3Yzc0|60f6dde59909a5c7efd954d09fbc2623aa9ca6e303409ccebee0c49cfec25bf1"',
-    'user_nickname': '"2|1:0|10:1516465397|13:user_nickname|8:U2FpNjI4|6a67a839ea7fb9b1cc94102f62cf00624f99fd5b25b523be9f9b42d5cd80a1f4"',
-    'Hm_lpvt_85abf7488e97713e5a0e152b2ab2316d': '1516465398',
+    'Hm_lpvt_85abf7488e97713e5a0e152b2ab2316d': '1532933626',
+    'Hm_lvt_85abf7488e97713e5a0e152b2ab2316d': '1532933066',
+    'user_nickname': '"2|1:0|10:1532933138|13:user_nickname|8:U2FpNjI4|f19cba12c47546f3f2f937450fac37bfa45da4292c71307ad7c801466eb04494"',
+    'user_slug': '"2|1:0|10:1532933138|9:user_slug|8:YzI3Yzc0|82f6d08029b856098f9f26fa6001d199265d7960ff9b770387f778455b60420f"',
+    '_xsrf': '2|7dafcf54|c0dfd3d6cded55d1c16c0122d24f91f5|1532933065',
 }
