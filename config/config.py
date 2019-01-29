24a7260cebfe6a1655bc1b35813613d1d3278e35
Sai628 <sai100628@gmail.com>
2019-01-29 12:42:24 +0800

    update login cookies

--- config/config.py
+++ config/config.py
@@ -19,9 +19,9 @@ SIMULATE_HEADERS = {
 
 # 模拟已登录用户对应的Cookies
 LOGIN_COOKIES = {
-    'Hm_lpvt_85abf7488e97713e5a0e152b2ab2316d': '1532933626',
-    'Hm_lvt_85abf7488e97713e5a0e152b2ab2316d': '1532933066',
-    'user_nickname': '"2|1:0|10:1532933138|13:user_nickname|8:U2FpNjI4|f19cba12c47546f3f2f937450fac37bfa45da4292c71307ad7c801466eb04494"',
-    'user_slug': '"2|1:0|10:1532933138|9:user_slug|8:YzI3Yzc0|82f6d08029b856098f9f26fa6001d199265d7960ff9b770387f778455b60420f"',
-    '_xsrf': '2|7dafcf54|c0dfd3d6cded55d1c16c0122d24f91f5|1532933065',
+    'Hm_lpvt_85abf7488e97713e5a0e152b2ab2316d': '1548736642',
+    'Hm_lvt_85abf7488e97713e5a0e152b2ab2316d': '1548251672,1548562921',
+    'user_nickname': '"2|1:0|10:1548736641|13:user_nickname|8:U2FpNjI4|cc259a2f729b7016d6f81d086f1f1618514a0f3563128b2ad37133e815aad204"',
+    'user_slug': '"2|1:0|10:1548736641|9:user_slug|8:YzI3Yzc0|e833bd327a536f6e3efcd63f4eac2960cdc583992851aa64a0ee79649a543af1"',
+    '_xsrf': '2|1d48a1fb|db61923fe06765f0dc1dd9b41a9549c0|1548562918',
 }
